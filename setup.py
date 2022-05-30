cb14c41f48f9b693f69d7de3fba61a0aeb7d7185
Simon Markus <simon@jsmarkus.de>
2022-05-30 11:39:06 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='DSR Batch 30 portfolio project: identify, analyze and visualize physical processes in videos.',
+    author='Simon Markus',
+    license='',
+)
